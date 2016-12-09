7bb53700144b81b5a33723d560d2d2725dd88770
Grybyus <david.calistro@usach.cl>
2016-12-09 19:36:25 -0300

    primera subida

---
+++ src/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+void main(){
+	programa1();
+	programa2();
+}
---
+++ src/programa2.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+#define LARGO (536870912/2)
+
+int programa2(){
+	int i;
+	float array[20];
+	int x,*y;	
+	y=(int *)malloc( sizeof(int) *LARGO);
+	
+	clock_t start = clock();
+	for (i = 0; i < 20; i++)
+	{
+
+		for (x = 0; x < LARGO; x++)
+		{
+			y[rand()%LARGO] = x;
+
+		}
+		array[i]=((double)clock() - start) / CLOCKS_PER_SEC;
+		printf("Tiempo transcurrido: %f para iteración i =%i\n", ((double)clock() - start) / CLOCKS_PER_SEC,i);
+
+
+	}
+	free(y);
+
+	float varianza,desviaStandard,media = array[19]/(float)20;
+	varianza = (float)0;
+
+	for (i = 19; i > 0 ; i--)
+	{
+		array[i] = array[i] - array[i-1];
+		printf("Han pasado : %f sec, en la iteracion %i\n", array[i],i);
+	}
+		printf("Han pasado : %f sec, en la iteracion %i\n", array[0],0);
+	printf("La media es = %f \n", media);
+	
+	for (i = 19; i >= 0 ; i--)
+	{
+		varianza = varianza + powf((array[i] - media),2);
+	}
+	varianza = varianza/(float)20;
+	printf("Varianza : %f -\n",varianza);
+	desviaStandard = sqrtf(varianza);
+	printf("Desviacion Standard %f -\n",desviaStandard);
+
+	return 0;
+}
